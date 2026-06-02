422930c843a7bc5b8b0fae3483dfa8f15c568f2a
Christian Lund <clu@netic.dk>
2026-06-02 14:16:11 +0200

    bump aws - kubernetes provider version

--- versions.tf
+++ versions.tf
@@ -8,8 +8,8 @@
 terraform {
   required_version = ">= 0.14"
   required_providers {
-    aws = ">= 3.0"
-    kubernetes = ">= 2.0"
+    aws = ">= 6.0"
+    kubernetes = ">= 3.0"
     local = ">= 2.0"
   }
 }
